d437b1565c17844c32c772c0f5510207ba671c0c
Benjamin Capistrant <bencapistrant@gmail.com>
2020-04-27 13:08:09 -0400

    updating class 23

---
+++ public/23/d4ds_hex.R
@@ -0,0 +1,8 @@
+library(hexSticker)
+setwd("~/Google Drive File Stream/My Drive/2_SDS/SDS291/0_Spring2019")
+sticker("pchead.jpg", package="Dogs4DataScience", p_size=3.5, p_y=0.4, p_color = "#c47207", s_x=1.1, s_y=1.07, s_height=.48, s_width=.48, h_fill="#ffffff", h_color="#c47207",
+        filename="D4DShex_a.png")
+
+
+sticker("pchead.jpg", package="Dogs 4\nData Science", p_size=4.5,p_y=1.6, p_color = "#c47207", s_x=1.1, s_y=.85, s_height=.45, s_width=.45, h_fill="#ffffff", h_color="#c47207",
+        filename="D4DShex_b.png")
