47d54b0ac71d2556792a9a5299aa42c0e3cebb18
Benjamin Capistrant <bencapistrant@gmail.com>
2020-04-29 22:52:35 -0400

    updating exam

---
+++ public/exam/ncbirths_script.R
@@ -0,0 +1,12 @@
+library(openintro)
+library(tidyverse)
+data("ncbirths")
+
+ncbirths2 <- ncbirths %>%
+  mutate(lowCode = if_else(lowbirthweight=="low",1,
+                     if_else(lowbirthweight=="not low",0,NA_real_))) %>%
+  select(lowCode, lowbirthweight, mage, weeks, visits, marital,habit,whitemom) %>%
+  filter(complete.cases(.))
+
+ 
+write.csv(ncbirths2, file="~/Dropbox/3_Smith/SDS291/sds291_site/static/exam/291-2ndExam.csv")
